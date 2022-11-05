5055cccbf439ea13dd6e9182a582cbad5431c0b8
Giorgio Zett <giorgio.zett@gmail.com>
2022-11-05 13:43:43 +0100

    remove infinite handle game update scene

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-import { setup } from './services/BootGameService'
+import { setup } from "./services/BootGameService";
 
-import './styles/reset.css'
-import './styles/main.css'
-import './styles/loader.css'
+import "./styles/reset.css";
+import "./styles/main.css";
+import "./styles/loader.css";
 
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
-console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->')
+console.log(
+  "if you have sourcemaps enabled in your devtools, click on main.js:5 -->"
+);
 
 setup()
   .then((game) => {
-    document.querySelector('.loader-container').remove()
-    console.log('%cgame init done!!', 'color:green; font-size:30px;')
-    console.log(game)
+    document.querySelector(".loader-container").remove();
+    console.log("%cgame init done!!", "color:green; font-size:30px;");
+    console.log(game);
   })
-  .catch((exc) => console.error('game init failed', exc.message))
+  .catch((exc) => console.error("game init failed", exc.message));
--- src/services/BootGameService.js
+++ src/services/BootGameService.js
@@ -1,5 +1,5 @@
-import Phaser from 'phaser'
-import { create, preload, update } from './GameScenesService'
+import Phaser from "phaser";
+import { create, preload /*, update*/ } from "./GameScenesService";
 
 const config = {
   type: Phaser.AUTO,
@@ -8,18 +8,18 @@ const config = {
   scene: {
     preload: preload,
     create: create,
-    update: update,
+    // update: update,
   },
-}
+};
 
 export function setup() {
   return new Promise((resolve, reject) => {
     try {
       // TODO: clan code
-      const game = new Phaser.Game(config)
-      return resolve(game)
+      const game = new Phaser.Game(config);
+      return resolve(game);
     } catch (exc) {
-      reject(exc)
+      reject(exc);
     }
-  })
+  });
 }
