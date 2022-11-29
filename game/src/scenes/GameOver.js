fa4a6c8e51053fcbf69e27be04c63806a5c4e5af
gcoro <giulia994@ymail.com>
2022-11-29 21:40:30 +0100

    add links to game over

--- game/src/scenes/GameOver.js
+++ game/src/scenes/GameOver.js
@@ -22,9 +22,27 @@ class GameOver extends Phaser.Scene {
 		// guapen
 		const guapen = this.add.image(612, 236, "guapen");
 
+		// scp173
+		const scp173 = this.add.image(470, 520, "scp173");
+		scp173.scaleX = 1.4;
+		scp173.scaleY = 1.5;
+
+		// scp5153
+		const scp5153 = this.add.image(655, 520, "scp5153");
+		scp5153.scaleX = 1.4;
+		scp5153.scaleY = 1.5;
+
 		// guapen (components)
 		const guapenPushOnClick = new PushOnClick(guapen);
 		guapenPushOnClick.sceneToStartKey = "Level";
+
+		// scp173 (components)
+		const scp173PushOnClick = new PushOnClick(scp173);
+		scp173PushOnClick.sceneToStartKey = "url_173";
+
+		// scp5153 (components)
+		const scp5153PushOnClick = new PushOnClick(scp5153);
+		scp5153PushOnClick.sceneToStartKey = "url_5153";
 
 		this.events.emit("scene-awake");
 	}
